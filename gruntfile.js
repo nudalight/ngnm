c865181e22d35133f3e28e2ba4a3b3218b69f863
nudalight <nuda.light@gmail.com>
2016-08-23 16:54:05 +0300

    start to configure gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -3,6 +3,31 @@ module.exports = function(grunt){
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    concat: {
+      ts: {
+        options: {
+          separator: '\n'
+        },
+        src: [
+          './dev/client/app/*.ts',
+          './dev/client/app/**/*.ts'
+        ],
+        dest: './dev/grunt/.temp/client-middle.ts'
+      },
+      sass: {
+        options: {
+          separator: '\n'
+        },
+        src: [
+          './dev/client/app/shared/*.sass',
+          './dev/client/app/**/*.sass'
+        ],
+        dest: './dev/grunt/.temp/client-middle.sass'
+      }
+
+    },
+
+
     copy: {
       images: {
         src: ['./dev/client/assets/img/**'],
@@ -25,6 +50,28 @@ module.exports = function(grunt){
       }
     },
 
+    typescript: {
+      base: {
+        options: {
+          sourceMap: true
+        },
+        src: [
+          './dev/client/app/*.ts',
+          './dev/client/app/**/*.ts'
+        ],
+        dest: './client/js/app.js'
+      }
+    },
+
+    uglify: {
+      client: {
+        options: {
+          screwIE8: false,
+          drop_console: true
+        }
+      }
+    },
+
     connect: {
       server: {
         options: {
@@ -39,6 +86,20 @@ module.exports = function(grunt){
   });
 
 
+  grunt.registerTask('compile:js', ['typescript', 'uglify']);
+
+  grunt.registerTask('fire', () => {
+    console.log('FIRE!');
+
+    var x = grunt.file.read('./dev/client/app/**/*.ts');
+    console.log(x);
+
+
+
+  });
+
+
+  grunt.loadNpmTasks('grunt-typescript');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -47,6 +108,5 @@ module.exports = function(grunt){
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-sass');
-
 
 };
